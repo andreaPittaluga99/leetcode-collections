8e2003cab0b68824baa14a3e35e2ee6792d757f8
Andrea Pittaluga <andrea.pittaluga.99@gmail.com>
2026-08-28 18:14:18 +0200

    Update 58.cpp

--- easy/58.cpp
+++ easy/58.cpp
@@ -5,8 +5,8 @@ class Solution {
 public:
     int lengthOfLastWord(std::string s) {
         int ret = 0;
-        for (size_t i = s.length() - 1; i >= 0; i--) {
-            if(s[i] == ' ') {
+        for (size_t i = s.length(); i > 0; i--) {
+            if(s[i - 1] == ' ') {
                 if (ret > 0 ) break;
             }
             else ret++;
