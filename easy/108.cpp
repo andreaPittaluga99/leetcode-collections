1be4be3f7233e31b0e42564f786d0073781c8b98
Andrea Pittaluga <andrea.pittaluga.99@gmail.com>
2026-09-03 19:10:53 +0200

    Create 108.cpp

---
+++ easy/108.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ #include <vector>
+
+ struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+ };
+
+class Solution {
+public:
+    TreeNode* aux(std::vector<int>& nums, int left, int right) {
+        if (left > right) return nullptr;
+
+        int mid = left + (right - left) / 2;
+        TreeNode* root = new TreeNode{nums[mid], aux(nums, left, mid - 1), aux(nums, left, mid - 1)};
+
+        return root;
+    }
+
+    TreeNode* sortedArrayToBST(std::vector<int>& nums) {
+        return aux(nums, 0, nums.size() - 1);
+    }
+};
