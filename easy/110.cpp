f25a7a0048fbebd21af9402108cf87a14391b8c3
Andrea Pittaluga <andrea.pittaluga.99@gmail.com>
2026-09-04 23:22:50 +0200

    Create 110.cpp

---
+++ easy/110.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include <math.h>
+
+class Solution {
+public:
+
+    int getHeight(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        return 1 + std::max(getHeight(root->left), getHeight(root->right));
+    }
+
+    bool isBalanced(TreeNode* root) {
+        if (root == nullptr) return true;
+
+        int leftHeight = getHeight(root->left);
+        int rightHeight = getHeight(root->right);
+
+        if (std::abs(leftHeight - rightHeight) > 1) return false;
+
+        return isBalanced(root->left) && isBalanced(root->right);
+    }
+};
